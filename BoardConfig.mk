bf5d78c111a789344df708e67cc4a40fbcdb1119
Joshndroid <fmxjoshiewah@gmail.com>
2015-11-09 16:30:45 +1000

    Add in RPI specific toolchain
    Device requires different toolchain
    change is not 100% there are we need to change the default from arm-eabi
    
    Signed-off-by: Joshndroid <fmxjoshiewah@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -32,6 +32,8 @@ TARGET_CPU_SMP := true
 #kernel
 TARGET_KERNEL_SOURCE := kernel/rpi
 TARGET_KERNEL_CONFIG := bcm2709_defconfig
+TARGET_KERNEL_GCC_ARM := 4.9.3
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/linux-x86/arm/arm-linux-gnueabihf-$(TARGET_KERNEL_GCC_ARM)/bin
 
 TARGET_RUNNING_WITHOUT_SYNC_FRAMEWORK := true
 
